f0ff0c7bf09e6d30d36fe10b807a8a073d0854fc
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2023-04-26 17:41:59 -0400

    some adjustability stuff

--- coolquestclient.js
+++ coolquestclient.js
@@ -2,20 +2,22 @@
 
 function setup() {
 	//createCanvas(800, 600);
-	createCanvas(600, 450);
+	createCanvas(650, 500);
 	textAlign(LEFT, TOP);
 	
 	let tileRes = 50
+	let tileX = 10
+	let tileY = 10
 
-	for( let i = 0; i < 9; i++ ) {
-		for( let j = 0; j < 9; j++ ) {
-			fill(0x00, 0xAF, 0x00)
+	for( let i = 0; i < tileY; i++ ) {
+		for( let j = 0; j < tileX; j++ ) {
+			fill(255)
 			rect(j*tileRes, i*tileRes, tileRes, tileRes)
 		}
 	}
 	fill(128)
-	rect(9*tileRes, 0, 3*tileRes, 9*tileRes)
-	let textRef = [tileRes*9, 0]
+	rect(tileX*tileRes, 0, 3*tileRes, tileY*tileRes)
+	let textRef = [tileRes*tileX, 0]
 	textSize(24)
 	fill(0)
 	text("CoolQuest", textRef[0], textRef[1])
